26e78666f5fba194e57f4088b7926fde89bfab00
Rachel Cole <rcole9@student.ccp.edu>
2026-04-16 17:38:37 +0000

    Remove PhaserGame component and revert to using GamePlayClient in the main page

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import GamePlayClient from "@/components/GamePlayClient";
 
-import dynamic from "next/dynamic";
-const PhaserGame = dynamic(() => import("@/components/PhaserGame"), { ssr: false });
-
 export default function Page() {
-  return <PhaserGame />;
+  return <GamePlayClient />;
 }
